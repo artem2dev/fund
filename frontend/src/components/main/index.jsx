debd80b1a3f41bb4c46358e064b8a8a56dea600f
Zalim <50583447+zabivan69@users.noreply.github.com>
2023-07-06 14:06:29 +0300

    Update index.jsx

--- frontend/src/components/main/index.jsx
+++ frontend/src/components/main/index.jsx
@@ -9,34 +9,34 @@ export default function MainPage({ news }) {
 				flexDirection={
 					document.documentElement.clientWidth > 767 ? "row" : "column"
 				}
-				justifyContent={"space-between"}
+				justifyContent={document.documentElement.clientWidth > 767 ? "flex-end" : "center" }
 			>
-				<Box
-					fontSize={25}
-					fontWeight={600}
-					mt={"30px"}
-					mr={"30px"}
-					textAlign={"left"}
-					maxW={"650px"}
-				>
-					Целью нашего фонда является сбор средств для поддержки бойцов на
-					передовой - участников СВО. <br />
-					<br />
-					Мы оказываем всестороннюю поддержку солдатам и их семьям:
-					{
-						<UnorderedList>
-							<ListItem>
-								доставляем в зону боевых действий продукты питания
-							</ListItem>
-							<ListItem>лекарственные средства</ListItem>
-							<ListItem>транспортные средства</ListItem>
-							<ListItem>
-								оказываем правовую и юридическую поддержку лицам - участникам
-								боевых действий
-							</ListItem>
-						</UnorderedList>
-					}
-				</Box>
+// <Box
+// fontSize={25}
+// fontWeight={600}
+// mt={"30px"}
+// mr={"30px"}
+// textAlign={"left"}
+// maxW={"650px"}
+// >
+// Целью нашего фонда является сбор средств для поддержки бойцов на
+// передовой - участников СВО. <br />
+// <br />
+// Мы оказываем всестороннюю поддержку солдатам и их семьям:
+// {
+//   <UnorderedList>
+//     <ListItem>
+//       доставляем в зону боевых действий продукты питания
+//     </ListItem>
+//     <ListItem>лекарственные средства</ListItem>
+//     <ListItem>транспортные средства</ListItem>
+//     <ListItem>
+//       оказываем правовую и юридическую поддержку лицам - участникам
+//       боевых действий
+//     </ListItem>
+//   </UnorderedList>
+// }
+// </Box>
 				<Box display={"flex"} justifyContent={"center"}>
 					<Box
 						maxW={500}
