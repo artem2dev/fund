0ae946f09984c376af7ba9d6b9972946d99e962e
Zalim <50583447+zabivan69@users.noreply.github.com>
2023-07-06 11:11:28 +0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,7 +11,7 @@ import { join } from 'path';
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '..', 'media'),
-      renderPath: '/media',
+      renderPath: '/media/',
     }),
     AuthModule, 
     TypeOrmModule.forRoot(OrmConfig)],
