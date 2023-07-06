094594322ca64b656986f97c2a8f96418d9067a1
Zalim <50583447+zabivan69@users.noreply.github.com>
2023-07-06 11:40:29 +0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,10 +22,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  app.useStaticAssets(join(__dirname, '..', '..', 'media'), {
-    index: false,
-    prefix: 'api/media',
-  });
+  // app.useStaticAssets(join(__dirname, '..', '..', 'media'), {
+  //   index: false,
+  //   prefix: 'api/media',
+  // });
 
   await app.listen(PORT, () =>
     logger.log(`App successfully started on port ${PORT}`),
